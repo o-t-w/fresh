46d1a1054c300727b13a859a08be873f8abf5fb8
Oliver Williams <oliverwilliams345@gmail.com>
2022-11-26 22:12:07 +0000

    try using dynamic viewport height unit

--- routes/index.tsx
+++ routes/index.tsx
@@ -30,7 +30,7 @@ export default function Home() {
 
       </Head>
     <div class="vh opening figjam">
-    <svg style="position: absolute; inset: 0; margin: auto;" width="600" height="164" viewBox="0 0 600 164" fill="none" xmlns="http://www.w3.org/2000/svg">
+    {/* <svg style="position: absolute; inset: 0; margin: auto;" width="600" height="164" viewBox="0 0 600 164" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_1007394_25387)">
 <rect x="7.99119" y="7.99119" width="584.017" height="148.41" stroke="var(--purple)" stroke-width="2.28324"/>
 <path d="M1.14162 1.14162H17.1243V17.1243H1.14162V1.14162Z" fill="white" stroke="var(--purple)" stroke-width="2.28324" stroke-linecap="square"/>
@@ -43,7 +43,24 @@ export default function Home() {
 <rect width="600" height="164" fill="white"/>
 </clipPath>
 </defs>
+</svg> */}
+
+<svg style="position: absolute; inset: 0; margin: auto; scale: 1.1" width="537" height="128" viewBox="0 0 537 128" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_1007394_25387)">
+<rect x="6.07378" y="6.07329" width="524.853" height="115.792" stroke="#5651FF" stroke-width="1.73526"/>
+<path d="M0.86763 0.86763H13.0145V13.0145H0.86763V0.86763Z" fill="white" stroke="#5651FF" stroke-width="1.73526" stroke-linecap="square"/>
+<path d="M0.86763 114.924H13.0145V127.071H0.86763V114.924Z" fill="white" stroke="#5651FF" stroke-width="1.73526" stroke-linecap="square"/>
+<path d="M523.986 0.86763H536.133V13.0145H523.986V0.86763Z" fill="white" stroke="#5651FF" stroke-width="1.73526" stroke-linecap="square"/>
+<path d="M523.986 114.924H536.133V127.071H523.986V114.924Z" fill="white" stroke="#5651FF" stroke-width="1.73526" stroke-linecap="square"/>
+</g>
+<defs>
+<clipPath id="clip0_1007394_25387">
+<rect width="537" height="128" fill="white"/>
+</clipPath>
+</defs>
 </svg>
+
+
 
 
 
@@ -66,12 +83,11 @@ export default function Home() {
    <div class="postit"></div>
 </div>
 
-<div>
-  <div class="wireframes">
-    <h2 class="hand-written" style="font-weight: normal; font-size: 28px; margin-bottom: 12px;">Wireframes</h2>
-    <p class="redacted">At giffgaff and Gradle I've been responsible for building and maintaining design systems in Figma. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic vel consectetur, corporis ad molestias quis culpa explicabo ipsa modi ipsam labore? Molestiae dignissimos at consectetur iure, repellat asperiores obcaecati sequi.</p>
+<div style="max-width: 1400px; margin-inline: auto; padding-inline: 24px;">
+  <div class="wireframes" style="max-width: 700px;">
+  <h2 class="hand-written" style="font-weight: normal; font-size: 28px; margin-bottom: 12px;">Wireframes</h2>
+    <p class="redacted"><img style={{float: "left; width: 160px; margin-right: 32px; margin-bottom: 2px; margin-top: 12px"}} class="push-down" src="placeholder.svg" alt=""/>At giffgaff and Gradle I've been responsible for building and maintaining design systems in Figma. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic vel consectetur, corporis ad molestias quis culpa explicabo ipsa modi ipsam labore? Molestiae dignissimos at consectetur iure, repellat asperiores obcaecati sequi. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque delectus saepe nemo aliquam enim soluta debitis modi odio cupiditate iusto?</p>
     </div>
-    <img class="push-down" src="placeholder.svg" alt=""/>
 </div>
 
 <div class="vh visible-grid">
